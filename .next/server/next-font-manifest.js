b6286c2a340d2224011c95e8b6d4221362b67a0d
Lorenzo Parisi <61919904+lrnzdevza@users.noreply.github.com>
2024-01-24 00:03:50 +0200

    commit 2

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/lorenzoparisi/Documents/GitHub/happyhappy/src/app/layout\":[\"static/media/4c472ffaefe8ebfa-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
